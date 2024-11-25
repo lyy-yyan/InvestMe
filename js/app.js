76ddafc19f917476a61646e57c05a49e766caa86
lyy_yyan <lyy_yyan@163.com>
2024-11-25 16:18:36 +0800

    更新了样式表，添加了过渡效果；修改了主页标题与导航链接，新增了关于、项目页面；修正了问候语的拼写错误，并添加了文字淡入淡出效果和视频播放功能。

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/* 问好 */
 function getGreeting() {
     const now = new Date();
     const hour = now.getHours();
@@ -6,16 +7,16 @@ function getGreeting() {
     if (hour < 12) {
         greeting = "Good Morning!";
     } else if (hour < 18) {
-        greeting = "Goosng Afternoon!";
+        greeting = "Good Afternoon!";
     } else {
         greeting = "Good Evening!";
     }
 
     return greeting;
 }
-
 document.getElementById('greeting').textContent = getGreeting();
 
+/* 动态图片轮播 */
 document.addEventListener('DOMContentLoaded', () => {
     const images = document.getElementById('avatar');
     const imagePaths = ['img/user1.jpg', 'img/user2.jpg']; // 图片路径数组
@@ -29,3 +30,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     setTimeout(changeImage, 1000); // 初始延迟1秒后开始切换图片
 });
+
+/* 文字淡入淡出效果 */
+function fadeIn(element) {
+    element.style.opacity = 1; // 设置透明度为1（完全不透明）
+}
+function fadeOut(element) {
+    element.style.opacity = 0; // 设置透明度为0（完全透明）
+}
+const textElement = document.getElementById('greeting');
+fadeIn(textElement); // 调用fadeIn函数使文字淡入
+setTimeout(() => fadeOut(textElement), 5000); // 5秒后调用fadeOut函数使文字淡出
+
+/* 视频播放 */
+document.getElementById('playButton').addEventListener('click', function() {
+    var video = document.getElementById('myVideo');
+    document.getElementById('myVideo').textContent = '播放视频';
+    video.style.display = 'block'; // 显示视频
+    video.play(); // 播放视频
+});
+  
